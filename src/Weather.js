1168b53052c606f0dd470c8ca90349401a7b9766
annasitailo <sitajloanna@gmail.com>
2022-11-02 18:13:00 +0200

    Changed Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // import "./Weather.css";
 
 export default function Weather() {
-    const [city, setCity] = useState("");
+    const [city, setCity] = useState("Kyiv");
     const [loaded, setLoaded] = useState(false);
     const [weather, setWeather] = useState({});
 
@@ -32,32 +32,80 @@ export default function Weather() {
         setCity(event.target.value);
     }
 
-    let form = (
-        <div>
-            <h1>Forecast</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-            <button type="Submit">Search</button>
-            </form>
-        </div>
-    );
+    // let form = (
+    //     <div>
+    //         <h1>Forecast</h1>
+    //         <form onSubmit={handleSubmit}>
+    //             <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+    //         <button type="Submit">Search</button>
+    //         </form>
+    //     </div>
+    // );
 
-    if (loaded) {
+    // if (loaded) {
         return (
         <div>
-            {form}
-            <ul>
-            <li>Temperature: {Math.round(weather.temperature)}°C</li>
-            <li>Description: {weather.description}</li>
-            <li>Humidity: {weather.humidity}%</li>
-            <li>Wind: {weather.wind}km/h</li>
-            <li>
-                <img src={weather.icon} alt={weather.description} />
-            </li>
-            </ul>
+            <div className="header">
+                <h1 className="header-title">
+                    Forecast 🌡
+                </h1>
+                <hr />
+                <div className="row search">
+                    <form type="submit" onSubmit={handleSubmit} className="row col-10 search-form" id="search-form">
+                        <div className="col search-button">
+                            <button className="btn btn-info" id="search-button">
+                                Search
+                            </button>
+                        </div>
+                        <div className="col-10 search-field">
+                            <input
+                            type="search"
+                            id="city-input"
+                            className="search-line"
+                            placeholder="Type in your city"
+                            onChange={updateCity}
+                            />
+                        </div>
+                    </form>
+                    <div className="col current-location-button">
+                        <button className="btn btn-info" id="current-location-button">
+                            Current location
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="body">
+                <div className="row forecast-of-today">
+                    <div className="col location">
+                        <h1 className="city" id="city-name">{city}</h1>
+                    </div>
+                    <div className="col weather">
+                        <p className="date" id="current-date">
+                        </p>
+                        <p className="sun-status" id="sun-status">
+                            <img className="weather-icon" id="weather-icon" src={weather.icon} alt={weather.description} /><span id="weather-description">{weather.description}</span>
+                        </p>
+                        <p className="temperature" id="current-temperature">
+                            <span id="degrees">{Math.round(weather.temperature)}</span><a href="#" id="unit-of-measurement">°C</a>
+                        </p>
+                    </div>
+                </div>
+                <br />
+                <br />
+                <hr className="division" />
+                <br />
+                <br />
+                <div className="row forecasts" id="forecasts">
+                </div>
+            </div>
+            <div className="footer">
+                <h6 className="created-by">
+                    Created by Anna Sitailo <i className="col fa fa-github" id="github" aria-hidden="true"></i>: <a href="https://github.com/annasitail/homework4-react" target="_blank">github.com/annasitail</a>
+                </h6>
+            </div>
         </div>
         );
-    } else {
-        return form;
-    }
+    // } else {
+    //     return form;
+    // }
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Weather from './Weather';
 import reportWebVitals from './reportWebVitals';
 
 import "./Weather.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
